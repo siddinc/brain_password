02a548ebaf9a85c1c24b04cea073b8cc7296e7ab
siddharth <sanghavisidd@gmail.com>
2021-03-31 18:26:15 +0530

    update crud

--- app/crud/eeg_recordings.py
+++ app/crud/eeg_recordings.py
@@ -35,19 +35,13 @@ async def create_eeg_recordings_data(request: Request, user_id: UUID, eeg_files:
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="EEG Recordings for the user already exist")
 
   files = [await f.read() for f in eeg_files]
-  decoded_files = []
-  for file in files:
-    decoded_files.append([ j for row in file.decode("utf-8").strip().split("\n") for j in list(map(float, row.split(" ")))])
+  decoded_files = [list(map(lambda e: float(e), i.decode("utf-8").split(","))) for i in files]
   eeg_recordings = {"eeg_recording_" + str(i): j for i,j in enumerate(decoded_files, start=1)}
 
   new_eeg_recordings = await request.app.db[settings.eeg_recordings_collection].insert_one({
     **eeg_recordings,
     "user_id": user_id,
   })
-
-  if new_eeg_recordings.inserted_id is None:
-    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="EEG Recordings for the User not created")
-  return new_eeg_recordings
 
 
 async def update_eeg_recordings_data(request: Request, user_id: UUID, eeg: EEGRecordings):
