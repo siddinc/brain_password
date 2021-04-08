cdfb19709605e940145c4f5427dcd4841c26ac12
siddharth <sanghavisidd@gmail.com>
2021-04-08 20:18:35 +0530

    fix config bug

--- app/api/endpoints/prediction.py
+++ app/api/endpoints/prediction.py
@@ -6,9 +6,10 @@ from app.models.prediction import Prediction
 from app.crud.eeg_recordings import retrieve_all_eeg_recordings_data
 from app.core.utils import (get_spectrogram, load_network)
 from app.models.response import CustomResponse
+from time import time
 router = APIRouter()
 
-model = load_network("/home/parag/Desktop/BrainPasswordBackend/brain_password_backend/networks/model1.h5")
+model = load_network("/home/siddharth/Desktop/brain_password_backend/networks/model1.h5")
 print("Model loaded")  
 
 @router.post("/", status_code=status.HTTP_200_OK)
@@ -17,7 +18,10 @@ async def get_user_prediction(
   response: Response,
   eeg_file: UploadFile = File(...),
 ):
+  start = time()
   all_eeg_recordings = await retrieve_all_eeg_recordings_data(request)
+  end = time()
+  
   query_file = await eeg_file.read()
   decoded_file = list(map(lambda e: float(e), query_file.decode("utf-8").split(",")))
   query_recording = get_spectrogram(decoded_file, 54)
@@ -43,11 +47,12 @@ async def get_user_prediction(
       else:
          pred = np.squeeze(model.predict([dataset[:,0], dataset[:,1]]), axis = -1) 
          result = np.vstack((result, pred))   
-  result = np.argmin(np.mean(result, axis=0)) 
+  result = np.argmin(np.mean(result, axis=0))
+  
   
   return CustomResponse(
       status_code=status.HTTP_201_CREATED,
-      message="Subject recognized "+str(subjects[result])
+      message=str(end-start),
     )
     
 
